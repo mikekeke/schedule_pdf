a463ebd40c5ab682fc05c84039d170e12bf4db94
Mikhel <m282021@gmail.com>
2016-11-03 11:49:01 +0300

    c1

--- schedule/src/main/java/ru/mikekekeke/kostromatransport/schedule/Settings.java
+++ schedule/src/main/java/ru/mikekekeke/kostromatransport/schedule/Settings.java
@@ -10,6 +10,4 @@ public final class Settings {
     public static final String JSON_LINK = "http://pastebin.com/raw/Kt2TwPrA";
     public static final String FILE_EXT = ".pdf";
     public static final String ssFILE_EXT = ".pdf";
-    public static final String dd = ".pdf";
-
 }
